975fc9fefc47ff1ea1d79b106b25048a2c4601b6
emonShimoul <emonshimoul@gmail.com>
2022-11-23 01:24:01 +0600

    redesign the table

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
                 </Form.Group>
 
                 <Form.Select className='w-50' aria-label="Default select example">
-                    <option>Select States</option>
+                    <option>Select State</option>
                     <option value="todo">TODO</option>
                     <option value="inprogress">In Progress</option>
                     <option value="done">Done</option>
@@ -39,11 +39,12 @@ const Home = () => {
 
             <div id="items" className="my-4">
                 <h3>Your Items</h3>
-                <table className="table mt-4">
+                <table className="table mt-4 text-start">
                     <thead>
                         <tr>
                             <th scope="col">Item Title</th>
                             <th scope="col">Item Description</th>
+                            <th scope="col">States</th>
                             <th scope="col">Action</th>
                         </tr>
                     </thead>
@@ -51,7 +52,15 @@ const Home = () => {
                         <tr>
                             <td>Get Some Coffee</td>
                             <td>You need coffee as u r a coder</td>
-                            <td><button className="btn btn-sm btn-primary">Delete</button></td>
+                            <td>
+                                <Form.Select className='w-50' aria-label="Default select example">
+                                    <option>Change</option>
+                                    <option value="todo">TODO</option>
+                                    <option value="inprogress">In Progress</option>
+                                    <option value="done">Done</option>
+                                </Form.Select>
+                            </td>
+                            <td><button className="btn btn-sm btn-danger ms-2">Delete</button></td>
                         </tr>
                     </tbody>
                 </table>
